e322c4606a084784f556fe343ab2c31cc72457bf
Mike McGann <mike.mcgann@blackchip.org>
2017-08-07 17:19:16 -0400

    oper tests

--- internal/fntest/channel_modes_test.go
+++ internal/fntest/channel_modes_test.go
@@ -62,5 +62,82 @@ func TestModeDeOper(t *testing.T) {
 			t.Fatalf("\n want: %v \n have: %v", want, have)
 		}
 	}
+}
 
+func TestModeOperNoAction(t *testing.T) {
+	s, c := tester.NewServer(t)
+	defer s.Quit()
+
+	c.Login("Batman", "batman 0 * :Bruce Wayne").Join("#gotham")
+	c.Send("MODE #gotham +o Robin")
+	c.Send("PING hello")
+	c.WaitFor(irc.PongCmd)
+	if c.Err() != nil {
+		t.Errorf("\n want: no error \n have: %v", c.Err())
+	}
+}
+
+func TestModeOperNoUser(t *testing.T) {
+	s, c := tester.NewServer(t)
+	defer s.Quit()
+
+	c.Login("Batman", "batman 0 * :Bruce Wayne").Join("#gotham")
+	c.Send("MODE #gotham +o Robin")
+	c.Send("PING hello")
+	c.WaitFor(irc.PongCmd)
+	if c.Err() != nil {
+		t.Errorf("\n want: no error \n have: %v", c.Err())
+	}
+}
+
+func TestModeOperNoUserInChannel(t *testing.T) {
+	s, c := tester.NewServer(t)
+	defer s.Quit()
+	c2 := s.NewClient()
+
+	c.Login("Batman", "batman 0 * :Bruce Wayne").Join("#gotham")
+	c2.Login("Robin", "robin 0 * :Boy Wonder").Join("#batcave")
+
+	c.Send("MODE #gotham +o Robin")
+	c.Send("PING hello")
+	c.WaitFor(irc.PongCmd)
+	if c.Err() != nil {
+		t.Errorf("\n want: no error \n have: %v", c.Err())
+	}
+}
+
+func TestModeOperNotOper(t *testing.T) {
+	s, c := tester.NewServer(t)
+	defer s.Quit()
+	c2 := s.NewClient()
+	c3 := s.NewClient()
+
+	c.Login("Batman", "batman 0 * :Bruce Wayne").Join("#gotham")
+	c2.Login("Robin", "robin 0 * :Boy Wonder").Join("#gotham")
+	c3.Login("Joker", "joker 0 * :The Joker").Join("#gotham")
+
+	c3.Drain()
+	c3.Send("MODE #gotham +o Robin")
+	c3.Send("NAMES #gotham")
+	have := AnyOf(c3.Recv(), "@Batman", "Robin", "Joker")
+	want := ":irc.localhost 353 X = #gotham :X X X"
+	if want != have {
+		t.Fatalf("\n want: %v \n have: %v", want, have)
+	}
+}
+
+func TestModeOperNoModeChange(t *testing.T) {
+	s, c := tester.NewServer(t)
+	defer s.Quit()
+	c2 := s.NewClient()
+
+	c.Login("Batman", "batman 0 * :Bruce Wayne").Join("#gotham")
+	c2.Login("Robin", "robin 0 * :Boy Wonder").Join("#gotham")
+
+	c.Send("MODE #gotham -o Robin")
+	c.Send("PING hello")
+	c.WaitFor(irc.PongCmd)
+	if c.Err() != nil {
+		t.Errorf("\n want: no error \n have: %v", c.Err())
+	}
 }
